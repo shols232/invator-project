be6fa60bcaca61e4d69ec48dd49d3759a30aaf4e
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-07-30 08:45:41 +0100

    Creating  models

--- invator/models.py
+++ invator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+"""
+Name of client
+Type of work done
+Amount to be payed
+Hours spent (optional)
+Amount by hour(if it was an hourly job)
+Amount by contract
+"""
+class Invator(models.Model):
+    Name = models.CharField(max_length=255,verbose_name='name')
+    Workdone = models.CharField(max_length=255)
+    Amount = models.CharField(max_length=255000000)
+    Hours = models.CharField(max_length=2500000)
+    HourAmount = models.CharField(max_length=255000000)
+    ContractAmount = models.CharField(max_length=255000000)
