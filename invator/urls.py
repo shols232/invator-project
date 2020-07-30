f0dc0d5af8bec61416303dc663433defe4490de1
Shols232 <62092484+shols232@users.noreply.github.com>
2020-07-30 18:40:45 +0100

    urls.py

--- invator/urls.py
+++ invator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.download_to_pdf, name="download_to_pdf",
+    path('', views.download_to_pdf, name="download_to_pdf",)
     path('/preview/<slug:id>', views.preview_template, name="preview_template")
 ]
